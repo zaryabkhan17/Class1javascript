18a4b48ecf273df3c21bdc8faeb9949bce8dc8c9
zaryabkhan17 <74405564+zaryabkhan17@users.noreply.github.com>
2021-01-01 15:22:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var name = "Mark";
+var Fname = "Mark k abbu";
+var Mname = "Mark ki ammi";
+alert( name);
+alert(Fname);
+alert(Mname);
+
+name = "Zaryab";
+ alert(name);
+ lk= "abs";
+ alert(lk);
+ 
